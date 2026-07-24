ad623467f774398bc29eab29fac938aaa6936387
mariblan <mariablancocanal@gmail.com>
2026-07-24 11:14:55 +0200

    Added Web Apis

--- 04-Web-APIs/lectures/src/main.js
+++ 04-Web-APIs/lectures/src/main.js
@@ -1,29 +1,40 @@
+const storeItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+const getItem = (key) => {
+  // const value = JSON.parse(localStorage.getItem(key));
+  // if(Array.isArray(value)) {
+  //   return value || []
+  // }
+  return JSON.parse(localStorage.getItem(key));
+}
+
 const form = document.querySelector('#add-todo');
 const todoList = document.getElementById('todo-list');
+let todos = localStorage.getItem("todos") || [];
 
-// console.log(form)
-// console.log(form[0])
-// console.log(todoList)
-const todos = [
-  {
-    id: `${new Date()}-cooking`,
-    text: 'Cooking',
-    complete: false,
-  },
-  {
-    id: `${new Date()}-singing`,
-    text: 'Singing',
-    complete: false,
-  }
-];
+// todos.forEach((todo) => console.log(todo));
+localStorage.setItem("username", "Alice")
+storeItem("username", "Alice")
+storeItem('age', 22);
+storeItem('isStudent', true);
+storeItem('hobbies', ['singing', 'running']);
+storeItem('person', {
+  name: 'Alice',
+  age: 22,
+  hobbies: ['singing', 'running'],
+});
+
+console.log(getItem("person"))
 
 const addTodo = (item) => {
   const li = document.createElement('li');
   li.id = item.id;
-  item.complete ? (li.style.textDecoration = 'line-through') : (li.style.textDecoration = 'none');
+  item.completed ? (li.style.textDecoration = 'line-through') : (li.style.textDecoration = 'none');
 
   const span = document.createElement('span');
-  span.textContent = item.text;
+  span.textContent = item.title;
 
   const deleteBtn = document.createElement('button');
   deleteBtn.textContent = 'delete';
@@ -35,20 +46,52 @@ const addTodo = (item) => {
   return li;
 };
 
-todos.forEach((todo) => {
-    const newTodoElement = addTodo(todo);
-    todoList.appendChild(newTodoElement)
-})
+// then() and catch()
+// fetch('https://jsonplaceholder.typicode.com/todos')
+// .then((response) => response.json())
+// .then((data) => {
+//   console.log(data.slice(0,10))
+//   data.slice(0,10).forEach((todo) => {
+//     const newTodoElement = addTodo(todo);
+//     todoList.appendChild(newTodoElement)
+//   })
+// })
+// .catch((error) => console.error(error))
+
+// async/ await
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+console.log(getData('https://jsonplaceholder.typicode.com/todos'));
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const inputValue = document.querySelector("#add-todo input").value.trim()
+  const inputValue = document.querySelector('#add-todo input').value.trim();
   const newTodoItem = {
-    id: new Date(),
-    text: inputValue,
-    complete: false
-  }
+    id: `${new Date()}-${inputValue}`,
+    title: inputValue,
+    completed: false,
+  };
   const newTodoElement = addTodo(newTodoItem);
   todoList.appendChild(newTodoElement);
-  form.reset()
+  form.reset();
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // const todos = await getData('https://jsonplaceholder.typicode.com/todos');
+  // console.log(todos);
+  todos.slice(0, 10).forEach((todo) => {
+    const newTodoElement = addTodo(todo);
+    todoList.appendChild(newTodoElement);
+  });
+
+  const photos = await getData('https://jsonplaceholder.typicode.com/photos');
+  console.log(photos.slice(0, 10));
 });
