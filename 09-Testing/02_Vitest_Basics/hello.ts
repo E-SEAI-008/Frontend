9d3244e20ff09962ce3133ec4c2ec4288437bb37
onureredo <onureredo@yahoo.com>
2026-09-15 10:56:52 +0200

    vitest basics

---
+++ 09-Testing/02_Vitest_Basics/hello.ts
@@ -0,0 +1,11 @@
+// export function sum(a: number, b: number): number {
+//   return a + b;
+// }
+
+// console.log(sum("10", 10));
+
+// function sum2(a, b) {
+//   return a + b;
+// }
+
+// console.log(sum2(10, '10'));
